53bc5cd2b3ecb2545b6e2b409ef1d2b266a6e60f
shahirzain <shahirzain100@gmail.com>
2019-02-13 22:32:15 +0500

    continue

--- src/component/testQuesJS1/testQuesJS1.js
+++ src/component/testQuesJS1/testQuesJS1.js
@@ -1,65 +1,295 @@
-import React, { Component } from 'react';
-import './testQuesJS1.css';
-import Div from './Ques/ques'
-import Input from './input/input'
-import history from '../../history';
-// import Result from './Result';
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+ import MCQs from "./mcq";
+/*import coverPic1 from "../../images/quiz.jpg";
+import coverPic2 from "../../images/test.png"; */
 
-
-class testQues extends Component {
-
-  state = {
-    html1ques: {
-      1: {
-        qid: 1,
-        ques: "HTML Stand for?",
-        opt1: "Hyper Type Markup Language",
-        opt2: "High Text Markup Language",
-        opt3: "Hyper Text Markup Language",
-        correct: ""
-      },
-
-      2: {
-        qid: 2,
-        ques: "Which tag is use for table ",
-        opt1: "<tr>",
-        opt2: "<table/>",
-        opt3: "<table> </table>",
-        correct: ""
-      },
-
-      three: {
-        qid: 3,
-        ques: "Where does the title tag define",
-        opt1: "in head tag",
-        opt2: "in body tag",
-        opt3: "in html tag",
-        correct: ""
-      }
-    },
-    result: null
+class QuizList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quiz_list: [
+        { quizName: "HTML", tests: 2 }
+      ],
+      quiz_info: [
+        {
+          quizName: "HTML",
+          tests: [
+            {
+              name: "Test 1",
+              questions: 5,
+              time: 60,
+              quiz_questions: [
+                {
+                  quiz: "What is abbreviation of HTML?",
+                  option1: "Hyper Type Multi Language",
+                  option2: "Higher Text Multiple Language",
+                  option3: "Hyper Text Markup Language",
+                  option4: "Hollow Type Markup Language ",
+                  answer: "3"
+                },
+                {
+                  quiz: "How many types of markup in HTML?",
+                  option1: "Both",
+                  option2: "1 - opening and closing markup only",
+                  option3: "1 - self closing markups only",
+                  option4: "None of above",
+                  answer: "1"
+                },
+                {
+                  quiz: "<iframe> is HTML5 markup",
+                  option1: "False",
+                  option2: "True",
+                  option3: "Neither true nor false",
+                  option4: "Not Sure",
+                  answer: "2"
+                },
+                {
+                  quiz: "<div> and <span> are inline elements?",
+                  option1: "True",
+                  option2: "False",
+                  option3: "Neither True nor false",
+                  option4: "None of above",
+                  answer: "3"
+                },
+                {
+                  quiz: "HTML must need body markup. Why? Because:",
+                  option1: "It did'nt needs to show the markups inside it.",
+                  option2: "It needs to show the markups inside it.",
+                  option3: "It needs <head> element",
+                  option4: "None of above",
+                  answer: "2"
+                }
+              ]
+            },
+            {
+              name: "Test 2",
+              questions: 10,
+              time: 120,
+              quiz_questions: [
+                {
+                  quiz:
+                    "The external JavaScript file must contain the <script> tag.",
+                  option1: "True",
+                  option2: "False",
+                  option3: "Neither true nor false",
+                  option4: "None of above",
+                  answer: "1"
+                },
+                {
+                  quiz:
+                    "Choose the correct HTML element for the largest heading:",
+                  option1: "<h6>",
+                  option2: "<heading>",
+                  option3: "<h1>",
+                  option4: "<head>",
+                  answer: "3"
+                },
+                {
+                  quiz:
+                    "What is the correct HTML element for inserting a line break?",
+                  option1: "<break>",
+                  option2: "<br>",
+                  option3: "<lnbr>",
+                  option4: "none of above",
+                  answer: "2"
+                },
+                {
+                  quiz:
+                    "What is the correct HTML for adding a background color?",
+                  option1: "<background>yellow</background>",
+                  option2: '<body style="bg-color:yellow;">',
+                  option3: '<body bg="yellow">',
+                  option4: '<body style="background-color:yellow;">',
+                  answer: "4"
+                },
+                {
+                  quiz:
+                    "Choose the correct HTML element to define important text",
+                  option1: "<strong>",
+                  option2: "<b>",
+                  option3: "<important>",
+                  option4: "<i>",
+                  answer: "1"
+                },
+                {
+                  quiz:
+                    "Choose the correct HTML element to define emphasized text",
+                  option1: "<i>",
+                  option2: "<emphasize>",
+                  option3: "<italic>",
+                  option4: "<em>",
+                  answer: "4"
+                },
+                {
+                  quiz: "What is the correct HTML for creating a hyperlink?",
+                  option1: "<a>http://www.w3schools.com</a>",
+                  option2: ' <a href="http://www.w3schools.com">W3Schools</a>',
+                  option3:
+                    '<a name="http://www.w3schools.com">W3Schools.com</a>',
+                  option4:
+                    '<a url="http://www.w3schools.com">W3Schools.com</a>',
+                  answer: "2"
+                },
+                {
+                  quiz: "Which character is used to indicate an end tag?",
+                  option1: "<",
+                  option2: "*",
+                  option3: "/",
+                  option4: "^",
+                  answer: "3"
+                },
+                {
+                  quiz: "How can you open a link in a new tab/browser window?",
+                  option1: '<a href="url" new>',
+                  option2: '<a href="url" target="_blank">',
+                  option3: '<a href="url" target="new">',
+                  option4: '<a href="url" target="new_blank">',
+                  answer: "2"
+                },
+                {
+                  quiz: "Which of these elements are all <table> elements?",
+                  option1: "<table><head><tfoot>",
+                  option2: "<table><tr><td>",
+                  option3: "<table><tr><tt>",
+                  option4: "<thead><body><tr>",
+                  answer: "2"
+                }
+              ]
+            }
+          ]
+        }      
+      ],
+      saveSelectedQuizObj: null,
+      renderSelectedTestObj: false,
+      renderMCQs: false,
+      currentTestIndex: null
+    };
+    this.back = this.back.bind(this);
+    this.backToDashboard = this.backToDashboard.bind(this);
   }
 
+  // saving selected quiz to state
+  updateQuizInfoState(index) {
+    const { quiz_info } = this.state;
+    this.setState({
+      saveSelectedQuizObj: quiz_info[index],
+      renderSelectedTestObj: true
+    });
+  }
 
-  render() {
-    let i=1
+  // back button function
+  back() {
+    this.setState({ renderSelectedTestObj: false });
+  }
+
+  backToDashboard(param) {
+    this.setState({ renderMCQs: param });
+  }
+
+  renderQuizInfo() {
+    const { saveSelectedQuizObj } = this.state;
     return (
       <div>
-        <div className="header">
-          <header>
-            <h3>JavaScript Quiz 1</h3>
-          </header>
+        <h2>{saveSelectedQuizObj.quizName} Quiz</h2>
+
+        <div className="row">
+          {saveSelectedQuizObj.tests.map((test, i) => {
+            return (
+              <div
+                className="col-md-4"
+                key={`${saveSelectedQuizObj.quizName}_${test.name}`}
+              >
+                <div className="card" style={{ width: "18rem" }}>
+                  <div className="card-body">
+                    <h5 className="card-title">{test.name}</h5>
+                    <p className="card-text">
+                      Total Questions: {test.questions}
+                    </p>
+                    <p>Total Time: {test.time / 60} Minutes</p>
+                    <button
+                      className="btn btn-success"
+                      onClick={() => {
+                        this.setState({
+                          renderMCQs: true,
+                          currentTestIndex: i,
+                          renderSelectedTestObj: false
+                        });
+                      }}
+                    >
+                      Start Quiz {i + 1} <i className=" fa fa-paper-plane" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <form>
-            <Div txt={this.state.html1ques[i].ques}/> <br/>
-            <h4>{this.state.html1ques[i].opt1}</h4> <br/>
-            <h4>{this.state.html1ques[i].opt2}</h4> <br/>
-            <h4>{this.state.html1ques[i].opt3}</h4>
-        </form>
+        <br />
+        <button className="btn btn-secondary" onClick={this.back}>
+          Back <i className="fa fa-backward" />
+        </button>
+      </div>
+    );
+  }
+
+  renderQuizList() {
+    const { quiz_list } = this.state;
+    return (
+      <div>
+        <h2>Dashboard</h2>
+
+        <div className="row">
+          {quiz_list.map((qList, index) => {
+            return (
+              <div className="col-md-4" key={`${qList}_${index}`}>
+                <div className="card" style={{ width: "18rem" }}>
+                  <div className="card-body">
+                    <h5 className="card-title">{qList.quizName} Quiz</h5>
+                    <p className="card-text">
+                      Test your skills of {qList.quizName} by taking this small quiz.
+                      It has {qList.tests} tests.
+                    </p>
+                    <button
+                      className="btn btn-info"
+                      onClick={this.updateQuizInfoState.bind(this, index)}
+                    >
+                      Next <i className=" fa fa-paper-plane" />
+                    </button>
+                    {/* <button className="btn btn-primary" >Next <i class=" fa fa-paper-plane"></i></button> */}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const {
+      renderSelectedTestObj,
+      renderMCQs,
+      saveSelectedQuizObj,
+      currentTestIndex
+    } = this.state;
+    return (
+      <div>
+        {renderSelectedTestObj ? (
+          this.renderQuizInfo()
+        ) : renderMCQs ? (
+          <MCQs
+            currentQuesObj={saveSelectedQuizObj}
+            currentTestIndex={currentTestIndex}
+            backToDashboard={this.backToDashboard}
+          />
+        ) : (
+          this.renderQuizList()
+        )}
+        {console.log("abcd",currentTestIndex, saveSelectedQuizObj, renderMCQs)}
       </div>
     );
   }
 }
 
-export default testQues;
+export default QuizList;
